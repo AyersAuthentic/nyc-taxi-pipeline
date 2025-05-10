ea9a21c497f0e4ae13ca4f5a4c93a5d32e615dfb
Kevin Ayers <home@Kevins-MacBook-Air.local>
2025-05-10 16:51:37 -0700

    Setup VPC using terraform, used module file structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,29 @@
 provider "aws" {
-    region = var.aws_region
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "terraform-nyc-taxi"
 
-    # Add default tags to all resources
-    default_tags {
-        tags = {
-            Project     = "nyc-taxi-pipeline"
-            Environment = "dev"
-            ManagedBy   = "terraform"
-        }
+  # Add default tags to all resources
+  default_tags {
+    tags = {
+      Project     = "nyc-taxi-pipeline"
+      Environment = "dev"
+      ManagedBy   = "terraform"
     }
+  }
 }
 
 # Configure backend for storing Terraform state
 terraform {
-    backend "s3" {
-        bucket = "nyc-taxi-pipeline-tfstate"
-        key    = "terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "terraform-state-lock"
-        encrypt        = true
-    }
+  backend "s3" {
+    bucket       = "nyc-taxi-pipeline-tfstate"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
-# Variables
-variable "aws_region" {
-    description = "AWS region to deploy resources"
-    type        = string
-    default     = "us-east-1"
+module "networking" {
+  source = "./modules/networking"
 }
-
