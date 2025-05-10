85057033fb6fe32eead1809a6fe11f87e876eede
Kevin Ayers <kevin.ayers123@icloud.com>
2025-05-10 16:51:37 -0700

    Setup VPC using terraform, used module file structure

---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,41 @@
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "nyc-taxi-vpc"
+}
+
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway"
+  type        = bool
+  default     = false
+}
+
+variable "single_nat_gateway" {
+  description = "Use a single NAT Gateway"
+  type        = bool
+  default     = false
+}
