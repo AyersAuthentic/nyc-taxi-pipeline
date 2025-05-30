e547bb40631a11a62b60a500ca0a37a8f36883ab
Kevin Ayers <kevin.ayers123@icloud.com>
2025-05-29 17:35:22 -0700

    terraform: debug secret errors, ran initial apply

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -26,19 +26,19 @@ variable "cidr_block" {
 variable "azs" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "private_subnets" {
   description = "List of private subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "public_subnets" {
   description = "List of public subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "enable_nat_gateway" {
