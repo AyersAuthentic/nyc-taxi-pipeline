35c5b283204d14f84a4c2f24e703ef54b5e0855e
Kevin Ayers <home@Kevins-MacBook-Air.local>
2025-08-06 19:28:02 -0700

    chore: added weather ingest payload that was removed

--- airflow/dags/elt_pipeline_dag.py
+++ airflow/dags/elt_pipeline_dag.py
@@ -74,7 +74,16 @@ with DAG(
     ingest_weather_data = LambdaInvokeFunctionOperator(
         task_id="ingest_noaa_weather_data",
         function_name=AWS_LAMBDA_FUNCTION_WEATHER,
-        payload=json.dumps({"start_date": "2024-03-01", "end_date": "2024-03-05"}),
+        payload=json.dumps(
+            {
+                "dataset_id": "GHCND",
+                "station_id": "GHCND:USW00094728",
+                "datatype_ids": "PRCP,TEMP,TAVG,TMAX,TMIN,WT16,WT14",
+                "start_date": "2024-03-01",
+                "end_date": "2024-03-05",
+                "units": "standard",
+            }
+        ),
         aws_conn_id=AWS_CONN_ID,
         region_name=AWS_REGION,
         do_xcom_push=True,
